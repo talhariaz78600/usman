8c279c522da6fe644079cb28105394d6310e87e9
Talha Riaz <muhammadtalha1005@gmail.com>
2023-06-05 22:58:36 +0500

    second

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const mongooseURI=process.env.mongooseURI;
 const connectToMongo=()=>{
-  mongoose.connect(mongooseURI, { useNewUrlParser: true })
+  mongoose.connect(`${mongooseURI}`, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error', err));
 }

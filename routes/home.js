a162aba1b5c4934890ee8930a2af3cdc1f7b7299
Talha Riaz <muhammadtalha1005@gmail.com>
2023-06-13 01:33:40 +0500

    how

--- routes/home.js
+++ routes/home.js
@@ -8,7 +8,6 @@ router.post('/homedata', async (req,res)=>{
     const contentId=req.body.contentId;
     const page=req.body.page;
     const success=false;
-
     try {
         
         const homedata = await Home.create({
@@ -23,12 +22,84 @@ router.post('/homedata', async (req,res)=>{
     }
 
 })
-router.get('/gethomedata', async (req,res)=>{
+router.get('/gethomeeducation', async (req,res)=>{
 
-    const page=req.header('page');
+    const title=req.header('title');
     let success=false;
     try {
-        const data=await Home.find({page});
+        const data=await Home.find({title});
+        success=true;
+        res.json({data,success});
+    } catch (error) {
+        res.json({error,success});
+    }
+})
+router.get('/gethomedepartment', async (req,res)=>{
+
+    const title=req.header('title');
+    let success=false;
+    try {
+        const data=await Home.find({title});
+        success=true;
+        res.json({data,success});
+    } catch (error) {
+        res.json({error,success});
+    }
+})
+router.get('/gethomeethics', async (req,res)=>{
+
+    const title=req.header('title');
+    let success=false;
+    try {
+        const data=await Home.find({title});
+        success=true;
+        res.json({data,success});
+    } catch (error) {
+        res.json({error,success});
+    }
+})
+router.get('/gethomefood', async (req,res)=>{
+
+    const title=req.header('title');
+    let success=false;
+    try {
+        const data=await Home.find({title});
+        success=true;
+        res.json({data,success});
+    } catch (error) {
+        res.json({error,success});
+    }
+})
+router.get('/gethomelove', async (req,res)=>{
+
+    const title=req.header('title');
+    let success=false;
+    try {
+        const data=await Home.find({title});
+        success=true;
+        res.json({data,success});
+    } catch (error) {
+        res.json({error,success});
+    }
+})
+router.get('/gethomesolutions', async (req,res)=>{
+
+    const title=req.header('title');
+    let success=false;
+    try {
+        const data=await Home.find({title});
+        success=true;
+        res.json({data,success});
+    } catch (error) {
+        res.json({error,success});
+    }
+})
+router.get('/gethometrade', async (req,res)=>{
+
+    const title=req.header('title');
+    let success=false;
+    try {
+        const data=await Home.find({title});
         success=true;
         res.json({data,success});
     } catch (error) {
@@ -36,4 +107,46 @@ router.get('/gethomedata', async (req,res)=>{
     }
 })
 
+
+router.delete('/deletehomedata/:id', async (req, res) => {
+    const id = req.params.id;
+    let success = false;
+    try {
+        const finddata = await Home.findByIdAndDelete(id);
+        if(!finddata){
+            return res.json({success,message:"Item not found"});
+        }
+        success = true;
+        res.json({ finddata, success })
+
+    } catch (error) {
+        res.json({ error, success })
+    }
+})
+
+////////////////////////////update education data/////////////////////////
+router.put('/updatehomedata/:id', async (req, res) => {
+    const title=req.body.title;
+    const contentId=req.body.contentId;
+    const page=req.body.page;
+    const success=false;
+    try {
+        const newData = {}
+        if (page) {
+            newData.page = page;
+        }
+        if (contentId) {
+            newData.contentId = contentId;
+        }
+        if (title) {
+            newData.title = title;
+        }
+        edu = await Home.findByIdAndUpdate(req.params.id, { $set: newData }, { new: true })
+        success = true;
+        res.json({ edu, success })
+    } catch (error) {
+        res.json({ error, success });
+    }
+
+})
 module.exports=router;
